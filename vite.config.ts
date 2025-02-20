a7b7f6aa184f3562d52ab9c24cebad65c2a5b622
dylanoh12 <143674689+dylanoh12@users.noreply.github.com>
2025-02-20 17:02:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/dylanoh12/MusicSettingsApp.git/', // Ensure this matches your GitHub repo name exactly
+  base: '/https://github.com/dylanoh12/MusicSettingsApp.git', // Ensure this matches your GitHub repo name exactly
   build: {
     outDir: 'dist',
   },
