1bff4c40b52ea31908e507c67a64ff1934b0ea1a
Dylanoh12 <maroonedsheep@gmail.com>
2025-02-20 13:30:30 +0800

    q

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/dylanoh12/MusicSettingsApp.git', // Change 'vite-app' to your repository name
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
