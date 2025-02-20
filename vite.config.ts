4badbf68c004d0c396424696228bbe3e865193b9
Dylanoh12 <maroonedsheep@gmail.com>
2025-02-20 13:41:38 +0800

    q1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/MusicSettingsApp/', // Ensure this matches your GitHub repo name exactly
+  base: 'https://github.com/dylanoh12/MusicSettingsApp.git', // Ensure this matches your GitHub repo name exactly
   build: {
     outDir: 'dist',
   },
